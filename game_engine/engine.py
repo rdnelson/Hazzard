d02fc9f2163737fc595b7684fe4020a92250230a
Robert Nelson <robert@rnelson.ca>
2013-12-02 09:43:19 -0500

    Added version one of powerups

--- game_engine/engine.py
+++ game_engine/engine.py
@@ -2,59 +2,229 @@
 
 import os
 import sys
+from threading import Timer
+from lxml import etree as xml
+from datetime import datetime, timedelta
 
 local_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append("%s/../PiNet" % local_dir)
 
 import PiNet
+import Powerups
+import laps
 
-DEBUG=1
+shutdown = False
+
+class PlayerState:
+    def __init__(self):
+        self.forward = 0
+        self.reverse = 0
+        self.speed_percent = DEFAULT_SPEED_PERCENT
+        self.turn = 0
+        self.immortal = False
+        self.effects = []
+        self.powerups = None
+        self.turn = 0
+        self.laps = []
+
+def TimeEvent():
+
+    # update effect status
+    for player in players:
+        handleEffects(player)
+
+    # insert powerup awarding here
+
+    if not shutdown:
+        timer = Timer(0.1, TimeEvent)
+        timer.start()
+
+
+def handleEffects(player):
+    for i in range(len(player.effects) - 1, -1, -1):
+        eff = player.effects[i]
+        timeElapsed = datetime.now() - eff[1]
+
+        #prune expired effects
+        if timeElapsed.seconds + timeElapsed.microseconds / 1000000.0 > eff[0].get_duration():
+            player.effects.pop(i)
+            continue
+
+        new_speed_percent = DEFAULT_SPEED_PERCENT
+        #if this executes, the effect is affecting the player.
+        #apply any speed modifiers
+        if eff[0].get_maxSpeed() != None:
+            if self.immortal or (self.immortal and eff[0].get_maxSpeed() > 0):
+                new_speed_percent += eff[0].get_maxSpeed()
+
+        player.speed_percent = new_speed_percent
+
+def triggerPowerup(player):
+
+    # Bail if there's no powerup
+    if players[player].powerup == None:
+        return
+
+    effects = players[player].powerup.get_effect()
+    if effects == None:
+        return
+
+    for target in getTargets(players[player].powerup, players.index(player)):
+        for eff in effects:
+            players[target].effects.append((eff,datetime.now()))
+
+def getTargets(powerup, caller_idx):
+
+    retVal = []
+
+    if powerup == None:
+        return retVal
+
+    rel = powerup.get_positionRel()
+    absol = powerup.get_positionAbs()
+    inv = powerup.get_invertSel()
+
+    # get current positions
+    positions = getPositions(players)
+    caller_pos = positions.index(caller_idx)
+
+    if absol != None:
+        # specific place (ex. 1st place)
+        if inv == None:
+            if absol < MAX_PLAYERS and absol >= 0:
+                if DEBUG:
+                    print "Targeting (Absolute): ", [positions[absol]]
+                return [positions[absol]]
+
+        # inverted placing (ex. all but 2nd)
+        if inv != None:
+            for i in range(MAX_PLAYERS):
+                if i != absol:
+                    retVal.append(positions[i])
+            if DEBUG:
+                print "Targeting (Inverse Absolute): ", retVal
+            return retVal
+
+    if rel != None:
+
+        target_pos = caller_pos - rel
+
+        # relative place (ex. 1 in front)
+        if inv == None:
+            if target_pos >= 0 and target_pos < MAX_PLAYERS:
+                if DEBUG:
+                    print "Targeting (Relative): ", [positions[target_pos]]
+                return [positions[target_pos]]
+
+        # relative inverted place (ex. all but me)
+        if inv != None:
+            for i in range(MAX_PLAYERS):
+                if i != target_pos:
+                    retVal.append(i)
+            if DEBUG:
+                print "Targeting (Inverse Relative): ", retVal
+            return retVal
+
+    return []
+
+#Define constants
+DEBUG=True
+
+#Event codes for xbox events
 LR = 0
 ACCEL = 5
 BRAKE = 2
-MAX_PLAYERS = 4
+BUT_A = 304
 
+#Game constants
+MAX_PLAYERS = 4
+MAX_SPEED = 255
+MIN_SPEED = -255
+DEFAULT_SPEED_PERCENT = 0.75
+
+#Validate the powerups schema
+powerups_schema_doc = xml.parse("./powerup.xsd")
+powerups_schema = xml.XMLSchema(powerups_schema_doc)
+powerups_doc = xml.parse("./powerups.xml")
+powerups_schema.assertValid(powerups_doc)
+
+# cleanup schema validation objects
+powerups_schema = None
+powerups_doc = None
+powerups_schema_doc = None
+
+#initialize pinet
 sender = PiNet.Sender()
 receiver = PiNet.Receiver()
 
-forward_speed = []
-rev_speed = []
-turn = []
+#load the powerup classes
+powerups = Powerups.parse("./powerups.xml").get_powerup()
+
+players = []
 
 for i in range(MAX_PLAYERS):
-    forward_speed.append(0)
-    rev_speed.append(0)
-    turn.append(0)
+    players.append(PlayerState())
 
 def ControllerEvent(player, event, data):
-    print "Received controller event"
+    if (DEBUG):
+        print "Received controller event"
+
     player = int(player)
+
+    #Bail if there's an invalid player
+    if (player < 0 or player > MAX_PLAYERS):
+        return
+
     event = int(event)
     data = int(data)
+
+    #don't send a packet unless necessary
     send = False
     if (event == ACCEL):
-        forward_speed[player] = data
+        players[player].forward = data
         send = True
     if (event == BRAKE):
-        rev_speed[player] = data
+        players[player].reverse = data
         send = True
     if (event == LR):
         send = True
         if data == -32767:
-            turn[player] = -1
+            players[player].turn = -1
         if data == 0:
-            turn[player] = 0
+            players[player].turn = 0
         if data == 32767:
-            turn[player] = 1
+            players[player].turn = 1
+    if event == BUT_A and data == 1 and players[player].powerup != None:
+        triggerPowerup(player)
 
     if send:
-        speed = forward_speed[player] - rev_speed[player]
-        if (DEBUG==1):
+        speed = players[player].forward - players[player].reverse
+
+        #Deal with player effects
+        handleEffects(player)
+
+        speed = int(speed * players[player].speed_percent)
+
+        #enforce speed boundaries
+        if (speed > MAX_SPEED):
+            speed = MAX_SPEED
+
+        if (speed < MIN_SPEED):
+            speed = MIN_SPEED
+
+        if (DEBUG):
             print "Player: %d, Speed=%d, Turn=%d" % (player, speed, turn[player])
         sender.sendAsync("CarEvent", player=str(int(player) + 1), speed=str(speed), turn=str(turn[player]))
-        print "Packet Sent"
+        if (DEBUG):
+            print "Packet Sent"
 
-print "Adding callback"
+print "Setting up PiNet connection"
 receiver.addCallback("ControllerEvent", ControllerEvent)
 
+timer = Timer(0.1, TimeEvent)
+timer.start()
+print "Beginning event timer"
+
 receiver.hibernate()
+shutdown = True
+print "Shutting down game engine."
---
+++ game_engine/laps.py
@@ -0,0 +1,36 @@
+def getPositions(players):
+    player_times = {}
+
+    for player in range(len(players)):
+        player_times[player] = players[player].laps
+
+    times = player_times.items()
+    times.sort(cmpPlayerLap)
+    return map(lambda item: item[0], times) # return the key (player #) in sorted order
+
+def _getPositions(player_times):
+    times = player_times.items()
+    times.sort(cmpPlayerLap)
+    return map(lambda item: item[0], times) # return the key (player #) in sorted order
+
+def cmpPlayerLap(x, y):
+    return cmpLap(x[1], y[1])
+
+def cmpLap(x, y):
+    # first finished more laps
+    if len(x) > len(y):
+        return -1
+    # second finished more laps
+    if len(x) < len(y):
+        return 1
+
+    # first is slower across all laps
+    if sum(x) > sum(y):
+        return 1
+
+    # first is faster across all laps
+    if sum(x) < sum(y):
+        return -1
+
+    # Same laps, same time.
+    return 0
